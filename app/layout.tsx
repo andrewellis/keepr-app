6c0162ad17e266ec501fe083aa06f0c82298d0d8
andrewellis <andrewellis@users.noreply.github.com>
2026-04-03 20:34:38 -0400

    Add Google Analytics 4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local'
 import './globals.css'
 import ServiceWorkerRegistration from '@/components/ServiceWorkerRegistration'
 import InstallBanner from '@/components/InstallBanner'
+import GoogleAnalytics from './GoogleAnalytics'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -45,6 +46,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics />
         {children}
         <ServiceWorkerRegistration />
         <InstallBanner />
